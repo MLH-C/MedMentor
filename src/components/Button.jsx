d9a79679f81b6a3b651d6ced983d50f35023be8f
Don Dang <105334355+dondangp@users.noreply.github.com>
2023-11-05 05:37:52 -0600

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,7 @@ import "./Button.css";
 
 const Button = ({ styles }) => (
   <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-    <button
-      type="button"
-      className={`button-glow py-4 px-6 font-poppins font-medium text-[18px] text-white bg-blue-gradient rounded-[10px] outline-none ${styles} `}
-    >
-      Get Started
-    </button>
+    
   </a>
 );
 
